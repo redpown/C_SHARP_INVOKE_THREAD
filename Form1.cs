c1b2a63cd4466c790b4f654b0f17dba49a87615f
André marques de lima <andre.marques091@gmail.com>
2023-09-13 14:27:57 -0300

    estudos

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        TesteThread teste;
+        public Boolean _isRun = false;
+        ThreadStart testeRun;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            teste = new TesteThread(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+          
+           // testeRun = new ThreadStart(delegate { teste.WriteTextSafe("Modificando o label."); });
+            //testeRun.Start();
+
+            //var threadParameters = new System.Threading.ThreadStart(delegate { teste.WriteTextSafe("This text was set safely."); });
+            var thread2  = new Thread(it => teste.Schedule());
+            thread2.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (teste._isRun == false)
+            {
+                teste._isRun = true;
+            }
+            else {
+                teste._isRun = false;
+            }
+        }
+    }
+}
---
+++ TesteThread.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class TesteThread : IDisposable
+    {
+        private Form1 Formulario;
+        private bool IsRunning;
+        public bool _isRun;
+        public TesteThread(Form1 Formulario)
+        {
+            IsRunning = true;
+            this.Formulario = Formulario;
+            //processor = new GhostscriptProcessor();
+        }
+        public void Dispose()
+        {
+            IsRunning = false;
+        }
+        //public void Looping()
+        //{
+        //    try
+        //    {
+        //        if (Formulario.label1.InvokeRequired)
+        //        {
+        //            // Call this same method but append THREAD2 to the text
+        //            Action safeWrite = delegate { WriteTextSafe($"{text} (THREAD2)"); };
+        //            Formulario.label1.Invoke(safeWrite);
+
+        //           // Formulario.label1.Text = "0000000000001";
+        //        }
+        //        //Formulario.Text = "0000000000009";
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //    }
+        //}
+
+        public void Schedule() {
+            int I = 0;
+            while (_isRun) {
+                Thread.Sleep(1000);
+                PopulateListView(I);
+                Thread.Sleep(5000);
+                I++;
+            }
+        }
+        public void WriteTextSafe(string text)
+        {
+        
+            if (Formulario.label1.InvokeRequired)
+            {
+                // Call this same method but append THREAD2 to the text
+                Action safeWrite = delegate { WriteTextSafe(text); };
+                //Formulario.label1.Text = "Modificando o label";
+                Formulario.label1.Invoke(safeWrite);
+
+            }
+            else
+            {
+              
+                Formulario.label1.Text = "iniciado o processo";
+                Formulario.Text = "Processando";
+
+                // Add the pet to our listview
+                string[] row = { "1", DateTime.Now.ToString() };
+                var listViewItem = new ListViewItem(row);
+
+
+
+                Formulario.listView1.Items.Add(listViewItem);
+
+
+            }
+        }
+
+        public void PopulateListView(int C)
+        {
+
+            if (Formulario.listView1.InvokeRequired)
+            {
+                // Call this same method but append THREAD2 to the text
+                Action PopulateList = delegate { PopulateListView(C); };
+                //Formulario.label1.Text = "Modificando o label";
+                Formulario.listView1.Invoke(PopulateList);
+
+            }
+            else
+            {
+
+                // Add the pet to our listview
+                string[] row = { C.ToString(), DateTime.Now.ToString() };
+                var listViewItem = new ListViewItem(row);
+
+                Formulario.listView1.Items.Add(listViewItem);
+
+
+            }
+        }
+
+    }
+}
